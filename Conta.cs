f38d2bc9ee78a6554816ba5dfaee6d4e25363b6e
br-bruno <bruno.ngu30@gmail.com>
2019-07-25 11:17:16 -0300

    Subindo projeto do bradesco

---
+++ Conta.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OiMundo
+{
+    class Conta
+    {
+       public double saldo;
+       public String titular;
+       public int numero;
+    public void Deposita(double valor)
+        {
+            this.saldo += valor;
+        }
+    public bool Saca(double valor)
+        {
+            if(this.saldo >= valor)
+            {
+                this.saldo -= valor;
+                return true;
+            }
+            return false;
+        }
+        
+    }
+}
--- Form1.cs
+++ Form1.cs
@@ -8,7 +8,8 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
-namespace Conta
+namespace OiMundo
+
 {
     public partial class Form1 : Form
     {
@@ -19,7 +20,31 @@ namespace Conta
 
         private void Button1_Click(object sender, EventArgs e)
         {
-            
+            Conta contaBruno = new Conta();
+            contaBruno.titular = "Bruno";
+            contaBruno.numero = 1;
+            contaBruno.Deposita(100.0);
+            MessageBox.Show("Saldo: " + contaBruno.saldo);
+            contaBruno.Saca(50.0);
+            MessageBox.Show("Saldo: " + contaBruno.saldo);
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Button2_Click(object sender, EventArgs e)
+        {
+            Conta mauricio = new Conta();
+            mauricio.saldo = 2000.0;
+
+            Conta copia = mauricio;
+            copia.saldo = 3000.0;
+
+            MessageBox.Show("mauricio = " + mauricio.saldo);
+            MessageBox.Show("copia = " + copia.saldo);
         }
     }
 }
